15e9a49e44aafdb42da0eeb3c4c18db26da9b929
Danielle Tobler <danielle.tobler210@gmail.com>
2020-04-20 21:34:02 -0500

    Added timeline/nav jump buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 "use strict";
 
-feather.replace();
+(function() {
+
+    $(document).ready(function() {
+
+
+        // setTimeout(function () {
+        //     $('#github-link').show();
+        // }, 200);
+        //
+        // setTimeout(function () {
+        //     $('#gitlab-link').show();
+        // }, 400);
+        //
+        // setTimeout(function () {
+        //     $('#linkedin-link').show();
+        // }, 600);
+        //
+        // setTimeout(function () {
+        //     $('#email-link').show();
+        // }, 800);
+
+    });
+
+})();
