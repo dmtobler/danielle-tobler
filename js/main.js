da9c5fc29ea48d68a9f0fd9c4fe496ac4b6f8ea2
Danielle Tobler <danielle.tobler210@gmail.com>
2020-04-25 19:08:35 -0500

    Added project div, need to integrate into timeline... commented out for now

--- js/main.js
+++ js/main.js
@@ -1,43 +1,18 @@
 "use strict";
 
 (function() {
-        // let mainContainer = $('#main-block-container');
-        // let containerHeight = $('#main-block-container').css('height');
-        // let nameWidth = $('#name-title').css('width');
-        // console.log(nameWidth);
-        // $(mainContainer).css('width', containerHeight);
 
-        let leftTimelineCirclesHeight = $('.left-timeline-circle').css('height');
-        let leftTimelineCirclesHeight = $('.left-timeline-circle').css('height');
+    let leftTimelineCirclesHeight = $('.left-timeline-circle').css('height');
+    let leftTimelineCirclesHeight = $('.left-timeline-circle').css('height');
 
     $(document).ready(function() {
 
 
         $(window).resize(function () {
-            mainContainer = $('#main-block-container');
-            containerHeight = $('#main-block-container').css('height');
+            let mainContainer = $('#main-block-container');
+            let containerHeight = $('#main-block-container').css('height');
             $(mainContainer).css('width', containerHeight);
         });
-
-
-
-
-
-        // setTimeout(function () {
-        //     $('#github-link').show();
-        // }, 200);
-        //
-        // setTimeout(function () {
-        //     $('#gitlab-link').show();
-        // }, 400);
-        //
-        // setTimeout(function () {
-        //     $('#linkedin-link').show();
-        // }, 600);
-        //
-        // setTimeout(function () {
-        //     $('#email-link').show();
-        // }, 800);
 
     });
 
